96876d51812bad299fa8869912b84b40d6dee41e
Francesco Sanna <fsanna@vivocha.com>
2016-03-22 10:59:49 +0100

    first commit

---
+++ resources/js/js.js
@@ -0,0 +1,13 @@
+/*
+
+jQuery(document).ready(function(){
+  //mobile menu toggling
+  $("#menu_icon").click(function(){
+    $("header nav ul").toggleClass("show_menu");
+    $("#menu_icon i").toggleClass("fa-bars");
+    $("#menu_icon i").toggleClass("fa-times");
+    return false;
+  });
+
+})
+*/
